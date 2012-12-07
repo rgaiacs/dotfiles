d3436912375db15be3eaf6023b75160f10de79ca
Raniere Silva <r.gaia.cs@gmail.com>
2012-12-07 18:33:36 -0200

    Add configuration of mutt.

--- chat.sh
+++ chat.sh
@@ -1,4 +1,11 @@
-#!/bin/sh
-tmux new-session -d -s chat 'newsbeuter'
-tmux splitw -p 50 'weechat-curses'
-tmux -2 attach-session -t chat
+#!/bin/bash
+if [ "$1" = 'windows' ]
+then
+	tmux new-session -d -s chat 'newsbeuter'
+	tmux new-window 'weechat-curses'
+	tmux -2 attach-session -t chat
+else
+	tmux new-session -d -s chat 'newsbeuter'
+	tmux splitw -p 50 'weechat-curses'
+	tmux -2 attach-session -t chat
+fi
