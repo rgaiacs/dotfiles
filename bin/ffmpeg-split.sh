e0a862af4b6e4ec5986e40139dff728ed1eb42ae
Raniere Silva <raniere@ime.unicamp.br>
2013-10-28 11:24:05 -0200

    Add script to split video

---
+++ bin/ffmpeg-split.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# -i filename (input)
+#      input file name
+# -t duration (output)
+#      Stop writing the output after its duration reaches duration.
+#      duration may be a number in seconds, or in "hh:mm:ss[.xxx]" form.
+#  -to position (output)
+#      Stop writing the output at position.  position may be a number in
+#      seconds, or in "hh:mm:ss[.xxx]" form.
+# -ss position (input/output)
+#          When used as an input option (before "-i"), seeks in this input file
+#          to position. When used as an output option (before an output
+#          filename), decodes but discards input until the timestamps reach
+#          position. This is slower, but more accurate.
+
+ffmpeg -i file_in file_out
