edeaf89b5a3552bb3ac7289574eaef4c4e0ec0a8
Raniere Silva <raniere@ime.unicamp.br>
2013-10-31 14:50:59 -0200

    Add mail list information

--- bin/mail-list-subscribe.sh
+++ bin/mail-list-subscribe.sh
@@ -12,4 +12,9 @@ echo "To: $1" >> ${FILE}
 echo "From: $2" >> ${FILE}
 echo "Subject: " >> ${FILE}
 
+# send email
 cat ${FILE} | msmtp -a $2 $1
+
+# add address to mutt configuration file
+echo "alias $1" >> ~/.mutt/mail-list-alias
+echo "subscribe $1" >> ~/.mutt/mail-list-address
